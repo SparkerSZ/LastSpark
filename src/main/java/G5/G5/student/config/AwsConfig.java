45f7d45136b6b8a04f36a9c579ff88f1a7e5fb56
Fuhao_Pan <fuhao_pan@epam.com>
2021-12-24 16:23:46 +0800

    aws s3 implementation

---
+++ src/main/java/G5/G5/student/config/AwsConfig.java
@@ -0,0 +1,45 @@
+package G5.G5.student.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Fuhao_Pan
+ * @date 12/24/2021
+ */
+@Configuration
+public class AwsConfig {
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+
+
+    @Bean
+    public AmazonS3 s3client() {
+
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey,secretKey);
+
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_EAST_2)
+                .build();
+
+
+
+        return  s3client;
+    }
+
+
+}
